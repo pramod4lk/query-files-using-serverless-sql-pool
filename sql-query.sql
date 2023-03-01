c80cd0a11156609e7dd2d61215fec865d28c0c49
Pramod Weerasinghe <pramod4lk@gmail.com>
2023-03-02 00:38:59 +0800

    Update sql-query.sql

--- sql-query.sql
+++ sql-query.sql
@@ -13,3 +13,30 @@ https://mydatalake.blob.core.windows.net/data/files/file*.csv: All .csv files in
 https://mydatalake.blob.core.windows.net/data/files/*: All files in the files folder.
 https://mydatalake.blob.core.windows.net/data/files/**: All files in the files folder, and recursively its subfolders.
 */
+
+
+/*Regardless of the type of delimited file you're using, you can read data from them by using the OPENROWSET function with the csv FORMAT parameter, 
+and other parameters as required to handle the specific formatting details for your data.
+
+SELECT TOP 100 *
+FROM OPENROWSET(
+    BULK 'https://mydatalake.blob.core.windows.net/data/files/*.csv',
+    FORMAT = 'csv',
+    PARSER_VERSION = '2.0',
+    FIRSTROW = 2) AS rows
+    
+/* Additional parameters you might require when working with delimited text files include:
+
+FIELDTERMINATOR - the character used to separate field values in each row. 
+    For example, a tab-delimited file separates fields with a TAB (\t) character. 
+    The default field terminator is a comma (,).
+ROWTERMINATOR - the character used to signify the end of a row of data. 
+    For example, a standard Windows text file uses a combination of a carriage return (CR) and line feed (LF), 
+    which is indicated by the code \n; while UNIX-style text files use a single line feed character, which can be indicated using the code 0x0a.
+FIELDQUOTE - the character used to enclose quoted string values. For example, 
+    to ensure that the comma in the address field value 126 Main St, 
+    apt 2 isn't interpreted as a field delimiter, you might enclose the entire field value in quotation marks like this: "126 Main St, apt 2". 
+    The double-quote (") is the default field quote character.
+ */
+ 
+ 
